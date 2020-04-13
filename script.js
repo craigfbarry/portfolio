584c37bb7e2bf2c709f3df9cacf0467685ce94a5
Craig Barry <craigfbarry@gmail.com>
2020-04-13 21:20:50 +0930

    Added a for loop to display projects with data/links etc from an array.

--- script.js
+++ script.js
@@ -4,27 +4,42 @@ $(document).ready(function () {
     $("#portfolio").on("click", function(){
         //Clear out content div and apply header.
         var projectDetails = [
-            {imageSource:"assets/horiseon.png",imageAlt:"code",description:"Craig's Homework",
+
+            {imageSource:"assets/weather-dashboard.png",imageAlt:"Weather-Dashboard",description:"Weather API Dashboard",
+            projectURL:"https://craigfbarry.github.io/weather-dashboard"},
+            {imageSource:"assets/password-generator.png",imageAlt:"Password-Generator",description:"Password Generator",
+            projectURL:"https://craigfbarry.github.io/password-generator/"},
+            {imageSource:"assets/horiseon.png",imageAlt:"horiseon",description:"Code Refactoring",
             projectURL:"https://craigfbarry.github.io/craigs_homework"},
+
+
            
         ];
-        
+        //Clear out contents and declaration of variables.
         $("#content").empty();
         var headerTag = $("<header>");
         var portfolioHeader = $("<h3>");
         var portfolioContent= $("<div>");
-        var project = $("<div>");
+        //var project = $("<div>");
+
+
         headerTag.addClass("row text-center text-align-middle");
         portfolioHeader.addClass("col my-4").text("Portfolio");
         $("#content").append((headerTag).append(portfolioHeader));
         portfolioContent.addClass("row text-center");
         $("#content").append(portfolioContent);
 
-        project.addClass("col-md-6 col-lg-4 px-0 px-md-4 bt-lg-5");
-        project.html("<img src=" + projectDetails[0].imageSource + " alt=" + projectDetails[0].imageAlt +  " width=100%>" +
-        "<a href=" + projectDetails[0].projectURL + "target='_blank'><p class='pt-4 pb-5 pb-md-5'>" + projectDetails[0].description + "</p></a>");
-        
-        portfolioContent.append(project);
+        for (var i=0;i<projectDetails.length;i++){
+            var project = $("<div>");
+            project.addClass("col-md-6 col-lg-4 px-0 px-md-4 bt-lg-5");
+            project.html("<img src=" + projectDetails[i].imageSource + " alt=" + projectDetails[i].imageAlt +  " width=100%>" +
+            "<a href=" + projectDetails[i].projectURL + " target='_blank'><p class='pt-4 pb-5 pb-md-5'>" + projectDetails[i].description + "</p></a>");
+            
+            portfolioContent.append(project);
+
+
+        }
+
 
 
 
